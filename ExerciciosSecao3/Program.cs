70ee55cc9b0f464f9f4d21a45a18b4839e044abf
Iuri Dias <iuridias.mw@gmail.com>
2017-07-30 00:40:33 -0300

    Projeto pronto

---
+++ ExerciciosSecao3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosSecao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRetangulo, alturaRetangulo, area, perimetro, diagonal;
+
+            baseRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            alturaRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = baseRetangulo * alturaRetangulo;
+            perimetro = 2 * (baseRetangulo + alturaRetangulo);
+            diagonal = Math.Sqrt(Math.Pow(baseRetangulo, 2) + Math.Pow(alturaRetangulo, 2));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Nome e idade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nome_e_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome1, nome2;
+            int idade1, idade2;
+            double mediaIdade;
+
+            string[] dados1;
+            string[] dados2;
+
+            dados1 = Console.ReadLine().Split(' ');
+            dados2 = Console.ReadLine().Split(' ');
+
+            nome1 = dados1[0];
+            idade1 = int.Parse(dados1[1]);
+
+            nome2 = dados2[0];
+            idade2 = int.Parse(dados2[1]);
+
+            mediaIdade = (idade1 + idade2) / 2.0;
+
+            Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + mediaIdade.ToString("F1", CultureInfo.InvariantCulture) + " anos");
+            Console.ReadLine();
+        }
+    }
+}
